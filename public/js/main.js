a97429207b1466c12caa49635e4a53dbba673f65
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2023-10-06 18:21:35 +0100

    advantages markup

--- public/js/main.js
+++ public/js/main.js
@@ -203,6 +203,11 @@
     window.isMobile = function () {
       return window.matchMedia("only screen and (max-width: 991px)").matches;
     };
+    var lazy = $('.lazy').Lazy({
+      effect: 'fadeIn',
+      effectTime: 500,
+      threshold: 0,
+    });
     new HeaderComponent().init();
 
     var debugInUrl = window.location.href.indexOf('debug') > -1;
--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,11 @@ $(function () {
   window.isMobile = function () {
     return window.matchMedia("only screen and (max-width: 991px)").matches;
   }
+  var lazy = $('.lazy').Lazy({
+    effect: 'fadeIn',
+    effectTime: 500,
+    threshold: 0,
+  });
   new HeaderComponent().init();
 
   var debugInUrl = window.location.href.indexOf('debug') > -1;
