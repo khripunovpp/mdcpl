e8dcfca082c7e1b0a85c82a27763c1442c7eef8b
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2023-10-07 15:17:09 +0100

    callback widget markup

--- public/js/main.js
+++ public/js/main.js
@@ -199,6 +199,37 @@
     that.navEleTopLevel$.removeClass('opened');
   };
 
+  var isFocus = function () {
+      var fieldEl = '.form__control',
+        groupEl = '.form__field',
+        labelEl = '.form__label',
+        onfocusClass = 'onfocus',
+        value;
+
+      $('body').addClass('js-placeholder');
+
+      $(fieldEl).each(function () {
+        value = $(this).val();
+        $(this).removeAttr('placeholder');
+        if (value.length > 0) $(this).closest(groupEl).addClass(onfocusClass).find(labelEl).fadeOut(200);
+      });
+
+      $(labelEl).on('click', function () {
+        $(this).closest(groupEl).find(fieldEl).focus();
+      });
+
+      $(fieldEl).on('focus', function () {
+        $(this).closest(groupEl).addClass(onfocusClass);
+      });
+
+      $(fieldEl).on('blur', function () {
+        value = $(this).val();
+        if (value.length == 0) $(this).closest(groupEl);
+        $(this).closest(groupEl).removeClass(onfocusClass);
+        if (value.length > 0) $(this).closest(groupEl).addClass(onfocusClass);
+      });
+    };
+
   $(function () {
     window.isMobile = function () {
       return window.matchMedia("only screen and (max-width: 991px)").matches;
@@ -210,6 +241,7 @@
     });
     new HeaderComponent().init();
 
+    isFocus();
     var debugInUrl = window.location.href.indexOf('debug') > -1;
     $('body').toggleClass('debug', debugInUrl);
   });
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,34 @@
 import {HeaderComponent} from "./_header";
+var isFocus = function () {
+    var fieldEl = '.form__control',
+      groupEl = '.form__field',
+      labelEl = '.form__label',
+      onfocusClass = 'onfocus',
+      value;
+
+    $('body').addClass('js-placeholder')
+
+    $(fieldEl).each(function () {
+      value = $(this).val();
+      $(this).removeAttr('placeholder')
+      if (value.length > 0) $(this).closest(groupEl).addClass(onfocusClass).find(labelEl).fadeOut(200);
+    })
+
+    $(labelEl).on('click', function () {
+      $(this).closest(groupEl).find(fieldEl).focus()
+    });
+
+    $(fieldEl).on('focus', function () {
+      $(this).closest(groupEl).addClass(onfocusClass)
+    });
+
+    $(fieldEl).on('blur', function () {
+      value = $(this).val();
+      if (value.length == 0) $(this).closest(groupEl)
+      $(this).closest(groupEl).removeClass(onfocusClass);
+      if (value.length > 0) $(this).closest(groupEl).addClass(onfocusClass);
+    });
+  }
 
 $(function () {
   window.isMobile = function () {
@@ -11,6 +41,7 @@ $(function () {
   });
   new HeaderComponent().init();
 
+  isFocus();
   var debugInUrl = window.location.href.indexOf('debug') > -1;
   $('body').toggleClass('debug', debugInUrl);
 });
